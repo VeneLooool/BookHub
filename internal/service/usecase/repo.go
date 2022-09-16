baa6a3fcdb1489fdcf9bc3615871727ec6699828
VeneLooool <timofej.gromb38@gmail.com>
2022-09-16 21:46:09 +0300

    simple repoService

--- internal/service/usecase/repo.go
+++ internal/service/usecase/repo.go
@@ -21,3 +21,39 @@ func NewRepoService(repoStorage RepoStorage) *RepoService {
 		storage: repoStorage,
 	}
 }
+
+func (rs *RepoService) CreateRepo(ctx context.Context, repo entity.Repo) (int64, error) {
+	return rs.storage.CreateRepo(ctx, repo)
+}
+func (rs *RepoService) GetRepo(ctx context.Context, ID int64) (entity.Repo, error) {
+	return rs.storage.GetRepo(ctx, ID)
+}
+func (rs *RepoService) updateRepo(oldRepo, newRepo entity.Repo) entity.Repo {
+	if newRepo.Visibility != "" {
+		oldRepo.Visibility = newRepo.Visibility
+	}
+	if newRepo.Name != "" {
+		oldRepo.Name = newRepo.Name
+	}
+	if newRepo.Desc != "" {
+		oldRepo.Desc = newRepo.Desc
+	}
+	return oldRepo
+}
+
+func (rs *RepoService) UpdateRepo(ctx context.Context, newRepo entity.Repo) (entity.Repo, error) {
+	repo, err := rs.GetRepo(ctx, newRepo.ID)
+	if err != nil {
+		return entity.Repo{}, err
+	}
+
+	repo = rs.updateRepo(repo, newRepo)
+	err = rs.storage.UpdateRepo(ctx, repo)
+	if err != nil {
+		return entity.Repo{}, err
+	}
+	return repo, nil
+}
+func (rs *RepoService) DeleteRepo(ctx context.Context, ID int64) error {
+	return rs.storage.DeleteRepo(ctx, ID)
+}
