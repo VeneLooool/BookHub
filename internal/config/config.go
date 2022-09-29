66ee87d50fb8c854e3c659ee00789e1844358e21
VeneLooool <timofej.gromb38@gmail.com>
2022-09-29 23:27:45 +0300

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,65 @@
 package config
+
+import (
+	"errors"
+	"github.com/spf13/viper"
+	"time"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+	Logger   Logger
+}
+
+type ServerConfig struct {
+	AppVersion   string
+	Port         string
+	Mode         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	Debug        bool
+}
+
+type Logger struct {
+	Development       bool
+	DisableCaller     bool
+	DisableStacktrace bool
+	Encoding          string
+	Level             string
+}
+
+type PostgresConfig struct {
+	PostgresqlHost     string
+	PostgresqlPort     string
+	PostgresqlUser     string
+	PostgresqlPassword string
+	PostgresqlDbname   string
+	PostgresqlSSLMode  bool
+	PgDriver           string
+}
+
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigFile(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+	return v, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
