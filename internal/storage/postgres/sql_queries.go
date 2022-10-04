6cc56033dd3f02f8b2f9897fbe22ba8b6d50c099
VeneLooool <timofej.gromb38@gmail.com>
2022-10-04 23:38:58 +0300

    work in progress on storage.pg.user

--- internal/storage/postgres/sql_queries.go
+++ internal/storage/postgres/sql_queries.go
@@ -10,5 +10,17 @@ const (
 						password,
 						user_desc
 				FROM users WHERE user_id = $1`
-	getReposForUser = ``
+	getReposForUser = `SELECT repo_id,
+								name,
+								visible,
+								description,
+								user_id
+				FROM repos WHERE user_id = $1`
+	updateUser = `UPDATE users
+					SET	name = COALESCE(NULLIF($1, ''), name),
+					username = COALESCE(NULLIF($2, ''), username),
+					password = COALESCE(NULLIF($3, ''), password),
+					user_desc = COALESCE(NULLIF($4, ''), user_desc)
+					WHERE user_id = $5`
+	deleteUser = `DELETE FROM users WHERE user_id = $1`
 )
--- internal/storage/postgres/user.go
+++ internal/storage/postgres/user.go
@@ -38,12 +38,45 @@ func (st *UserStorage) GetUser(ctx context.Context, ID int64) (user entity.User,
 	}
 	return user, nil
 }
-func (st *UserStorage) GetReposForUser(ctx context.Context, userID int64) ([]entity.Repo, error) {
-	return nil, nil
+func (st *UserStorage) GetReposForUser(ctx context.Context, userID int64) (repos []entity.Repo, err error) {
+	rows, err := st.db.QueryxContext(ctx, getReposForUser, &userID)
+	if err != nil {
+		return nil, fmt.Errorf("QueryxContext: %w", err)
+	}
+	for rows.Next() {
+		var repo entity.Repo
+		if err = rows.StructScan(&repo); err != nil {
+			return nil, fmt.Errorf("StructScan: %w", err)
+		}
+		repos = append(repos, repo)
+	}
+	return repos, nil
 }
-func (st *UserStorage) UpdateUser(context.Context, entity.User) error {
+func (st *UserStorage) UpdateUser(ctx context.Context, user entity.User) (err error) {
+	var u entity.User
+	if err = st.db.QueryRowxContext(ctx, updateUser,
+		&user.Name,
+		&user.UserName,
+		&user.Password,
+		&user.Desc,
+		&user.ID,
+	).StructScan(&u); err != nil {
+		return fmt.Errorf("QueryRowxContext: %w", err)
+	}
 	return nil
 }
-func (st *UserStorage) DeleteUser(context.Context, int64) error {
+func (st *UserStorage) DeleteUser(ctx context.Context, ID int64) (err error) {
+	result, err := st.db.ExecContext(ctx, deleteUser, ID)
+	if err != nil {
+		return fmt.Errorf("ExecContext: %w", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("RowsAffected: %w", err)
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("RowsAffceted: %w", entity.ErrUserNotFound)
+	}
 	return nil
 }
